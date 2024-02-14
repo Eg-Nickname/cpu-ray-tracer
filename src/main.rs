4c50f005fb2963298875f91df6391ad498f456a9
Alt+F4 <mail@example.com>
2024-02-14 01:40:00 +0100

    Moved different scenes to separate functions

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,39 @@ use cpu_ray_tracer::render;
 fn main(){
     let timer = Instant::now();
 
+    _mate_sphere();
+
+    
+    println!("Render time: {}", timer.elapsed().as_secs_f64());
+}
+
+fn _mate_sphere(){
+    let mut scene = Scene::new();
+    // Default 0 material
+    scene.add_material(material::Material::default());
+
+    // White emmisive sphere material
+    scene.add_material(material::Material::default());
+    scene.materials[1].albedo = Vec3::ONE;
+    scene.materials[1].emmision_color = Vec3::ONE;
+    scene.materials[1].emmision_power = 1.0;
+
+    // Purple mate sphere
+    scene.add_material(material::Material::default());
+    scene.materials[2].albedo = Vec3::new(0.8, 0.0, 1.0);
+
+    // White emissive sphere
+    scene.add_object(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -13.5), 10.0, 1)));
+    // Purple spheres
+    scene.add_object(Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5, 2)));
+    scene.add_object(Box::new(Sphere::new(Vec3::new(0.0, 100.5, -1.0), 100.0, 2)));
+
+
+    let render = render::Renderer::new(1000, 1000, scene);
+    render.render();
+}
+
+fn _emmisive_spheres_scene(){
     let mut scene = Scene::new();
     // Default 0 material
     scene.add_material(material::Material::default());
@@ -39,7 +72,4 @@ fn main(){
 
     let render = render::Renderer::new(1000, 1000, scene);
     render.render();
-
-    
-    println!("Render time: {}", timer.elapsed().as_secs_f64());
 }
